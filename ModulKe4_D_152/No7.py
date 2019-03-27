de2902974ef6a3fefa7433d21f2d9c43cbc3a701
L200170152 <47982293+L200170152@users.noreply.github.com>
2019-03-27 21:44:25 +0700

    Add files via upload

---
+++ ModulKe4_D_152/No7.py
@@ -0,0 +1,52 @@
+class MhsTIF(object):
+    def __init__(self, nama, nim, kota, us):
+        self.nama = nama
+        self.nim = nim
+        self.kota = kota
+        self.uangSaku = us
+    def __str__(self):
+        s = self.nama + ', nim ' + str(self.nim)\
+            + '. Tinggal di ' + self.kota\
+            + '. Uang saku Rp ' + str(self.uangSalu)\
+            + '. tiap bulannya.'
+        return s
+
+c0 = MhsTIF("Ika", 10, "Sukoharjo", 240000)
+c1 = MhsTIF("Budi", 51, "Sragen", 230000)
+c2 = MhsTIF("Ahmad", 2, "Surakarta", 250000)
+c3 = MhsTIF("Chandra", 18, "Surakarta", 235000)
+c4 = MhsTIF("Eka", 4, "Boyolali", 240000)
+c5 = MhsTIF("Fandi", 31, "Salatiga", 250000)
+c6 = MhsTIF("Deni", 13, "Klaten", 245000)
+c7 = MhsTIF("Galuh", 5, "Wonogiri", 245000)
+c8 = MhsTIF("Janto", 23, "Klaten", 245000)
+c9 = MhsTIF("Hasan", 64, "Karanganyar", 270000)
+c10 = MhsTIF("Khalid", 29, "Purwodadi", 265000)
+
+Daftar = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10]
+
+def binSeMass(kumpulan, target):
+    temp = []
+    low = 0
+    high = len(kumpulan)-1
+    while low <= high :
+        mid = (high+low)//2
+        if kumpulan[mid] == target:
+            midKiri = mid-1
+            while kumpulan[midKiri] == target:
+                temp.append(midKiri)
+                midKiri = midKiri-1
+            temp.append(mid)
+            midKanan = mid+1
+            while kumpulan[midKanan] == target:
+                temp.append(midKanan)
+                midKanan = midKanan+1
+            return temp
+        elif target < kumpulan[mid]:
+            high = mid-1
+        else:
+            low = mid+1
+    return False
+
+kumpulan = [2, 4, 5, 6, 6, 6, 8, 9, 9, 10, 11, 12, 13, 13, 14]
+print(binSeMass(kumpulan, 6))
